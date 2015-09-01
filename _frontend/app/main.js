ecdbe192e7c6b719843cd89922d668b10526e63e
Ye Yan <ye.yan@parashift.com.au>
2015-09-01 20:08:34 +0930

    Load code-prettify after add class, make sure the script pick up pre block correctly

--- _frontend/app/main.js
+++ _frontend/app/main.js
@@ -1,9 +1,9 @@
 $(function() {
     // use pretty print to render all the code snippet
     $("pre").addClass("prettyprint");
+    $.getScript("https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js");
 
     $(".archive-entry ul").hide();
-
     $(".archive-entry").click(function() {
         $(this).find("ul").toggle();
     });
